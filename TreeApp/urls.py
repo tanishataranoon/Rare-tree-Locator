4bf1e2402d4095ebc3b8e7fed68cc16d72783d03
Sajid Shahan Rahman <rahmansajid2985@gmail.com>
2025-09-23 03:48:11 +0600

    map integrate but no data showing

--- TreeApp/urls.py
+++ TreeApp/urls.py
@@ -4,4 +4,6 @@ from TreeApp import views as treeapp_views
 
 urlpatterns = [
     path('trees/', treeapp_views.TreeProfiles, name = 'TreeProfiles'),
+    path('add-tree-ajax/', treeapp_views.add_tree_ajax, name='add_tree_ajax'),
 ]
+
--- TreeApp/views.py
+++ TreeApp/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 # Create your views here.
+
+
+def homepage(request):
+    trees = TreeProfile.objects.all()
+    # Convert QuerySet to list of dicts
+    trees_data = list(trees.values('street_name', 'scientific_name', 'description', 'latitude', 'longitude'))
+    # JSON encode it safely
+    trees_json = json.dumps(trees_data, cls=DjangoJSONEncoder)
+    return render(request, 'HomePage.html', {'trees_json': trees_json})
 
 def TreeProfiles(request):
 
@@ -12,3 +24,15 @@ def TreeProfiles(request):
         }
     print(TreeProfiles)  
     return render(request, 'Trees/TreeProfiles.html', context=context)
+
+def add_tree(request):
+    if request.method == 'POST':
+        form = add_tree_ajax(request.POST)
+        if form.is_valid():
+            tree = form.save(commit=False)
+            tree.submitted_by = request.user
+            tree.save()
+            return redirect('home')  # or wherever you want
+    else:
+        form = add_tree_ajax()
+    return render(request, 'add_tree.html', {'form': form})

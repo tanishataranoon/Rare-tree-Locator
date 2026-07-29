e1ca2de59b7f51b0ac55e7e6e2ae44d57cc8f845
Sajid Shahan Rahman <rahmansajid2985@gmail.com>
2026-07-30 02:15:00 +0600

    kkala

--- staticfiles/JS/common/footer.js
+++
@@ -1,8 +0,0 @@
-// Auto-update year
-document.addEventListener("DOMContentLoaded", () => {
-  const yearEl = document.getElementById('year');
-  if (yearEl) {
-    yearEl.textContent = new Date().getFullYear();
-  }
-});
-

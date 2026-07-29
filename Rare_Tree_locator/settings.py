0c3f912eda95d85a04a8fcdc8fd6c0705bbcfba0
Sajid Shahan Rahman <rahmansajid2985@gmail.com>
2026-07-30 01:25:19 +0600

    m

--- Rare_Tree_locator/settings.py
+++ Rare_Tree_locator/settings.py
@@ -16,8 +16,8 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Set DEBUG = True via Render environment variables if you need to debug further
-# DEBUG = os.environ.get("DEBUG", "False") == "True"
-DEBUG = True  # Set to True for development, change to False in production
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+# DEBUG = True  # Set to True for development, change to False in production
 
 ALLOWED_HOSTS = ["*"]
 
